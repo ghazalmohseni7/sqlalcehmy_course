bb3d13b6f697cadc5af99e8b4c73b165913122a8
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-04-18 19:31:55 +0330

    fix : ORM : create a record with session

--- main_orm.py
+++ main_orm.py
@@ -1,1 +1,84 @@
+import sys
+import random
+import asyncio
+from faker import Faker
+import sqlalchemy as sqla
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from models import *
+from db import get_engine
+
+fake = Faker()
+
+if sys.platform:
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+async def insert_user_with_session():
+    engine = get_engine()
+    async_session = async_sessionmaker(bind=engine)  # this is a class
+    user = PyUser(
+        username=fake.word(),
+        age=random.randint(1, 100),
+        work=fake.word()
+    )  # this is a dataclass just for now
+    async with async_session() as session:
+        async with session.begin():
+            print("add user to session ")
+            session.add(user)  # now the user obj is in session memory / add is not async so no need for await
+            print("lets see inside the session memory:")
+            for key, obj in session.sync_session.identity_map.items():
+                print(f"{key}: {obj}")
+            print("/////////////////////////")
+            print("add user to db")
+            # await session.commit()  # this method will call the flush and commit together / actually cause we write .begine() adnd that is transactional , so we dont need the commit here
+            print("lets check to see if it is really in db or not:")
+            result = await session.execute(
+                sqla.select(PyUser).where(
+                    PyUser.username == user.username,
+                    PyUser.age == user.age,
+                    PyUser.work == user.work
+                )
+            )
+            row = result.first()
+            if row:
+                print(row._asdict())
+            print("lets refresh the session memory: specifically update this user we have ")
+            await session.refresh(user)
+            return user.to_dict()
+
+        """
+        (envsqlalchemy) D:\fastProject\sqlalcehmy_course>python main_orm.py
+        add user to session 
+        lets see inside the session memory:
+        /////////////////////////
+        add user to db
+        lets check to see if it is really in db or not:
+        2025-04-18 18:30:55,910 INFO sqlalchemy.engine.Engine select pg_catalog.version()
+        2025-04-18 18:30:55,910 INFO sqlalchemy.engine.Engine [raw sql] ()
+        2025-04-18 18:30:55,917 INFO sqlalchemy.engine.Engine select current_schema()
+        2025-04-18 18:30:55,917 INFO sqlalchemy.engine.Engine [raw sql] ()
+        2025-04-18 18:30:55,922 INFO sqlalchemy.engine.Engine show standard_conforming_strings
+        2025-04-18 18:30:55,922 INFO sqlalchemy.engine.Engine [raw sql] ()
+        2025-04-18 18:30:55,925 INFO sqlalchemy.engine.Engine BEGIN (implicit)
+        2025-04-18 18:30:55,927 INFO sqlalchemy.engine.Engine INSERT INTO sqla_user (username, age, work) VALUES ($1::VARCHAR, $2::INTEGER, $3::VARCHAR) RETURNING sqla_user.id
+        2025-04-18 18:30:55,927 INFO sqlalchemy.engine.Engine [generated in 0.00043s] ('chair', 77, 'quality')
+        2025-04-18 18:30:55,943 INFO sqlalchemy.engine.Engine SELECT sqla_user.id, sqla_user.username, sqla_user.age, sqla_user.work 
+        FROM sqla_user
+        WHERE sqla_user.username = $1::VARCHAR AND sqla_user.age = $2::INTEGER AND sqla_user.work = $3::VARCHAR
+        2025-04-18 18:30:55,943 INFO sqlalchemy.engine.Engine [generated in 0.00037s] ('chair', 77, 'quality')
+        {'PyUser': <models.PyUser object at 0x000002A80B9468C0>}
+        lets refresh the session memory: specifically update this user we have
+        2025-04-18 18:30:55,956 INFO sqlalchemy.engine.Engine SELECT sqla_user.id, sqla_user.username, sqla_user.age, sqla_user.work
+        FROM sqla_user
+        WHERE sqla_user.id = $1::INTEGER
+        2025-04-18 18:30:55,957 INFO sqlalchemy.engine.Engine [generated in 0.00054s] (2,)
+        2025-04-18 18:30:55,958 INFO sqlalchemy.engine.Engine COMMIT
+        main : insert_user_with_session :  <models.PyUser object at 0x000002A80B9468C0>
+
+        
+        """
+
+
+if __name__ == "__main__":
+    res = asyncio.run(insert_user_with_session())
+    print("main : insert_user_with_session : ", res)
--- models.py
+++ models.py
@@ -1,83 +1,94 @@
 from datetime import datetime, date
-from sqlalchemy import types, ForeignKey, Index, ForeignKeyConstraint , UniqueConstraint
+from sqlalchemy import types, ForeignKey, Index, ForeignKeyConstraint, UniqueConstraint
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from db import Base
 
 
-#
-# class PyUser(Base):
-#     # this is how we create table for sqlalchemy version >= 2.0
-#     __tablename__ = 'sqla_user'  # psql thinks table names are all lowercase so if you write uppercase letters it cant find them , so alway write tbale names in lower case
-#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)  # you should use python types
-#     username: Mapped[str] = mapped_column(types.String(100))  # VARCHAR(100)
-#     # 3 way to make a column nullable
-#     # age: Mapped[Union[int | None]] = mapped_column()
-#     age: Mapped[int | None] = mapped_column()
-#     work: Mapped[str] = mapped_column(nullable=True)  # TEXT
-#
-#     """
-#     CREATE TABLE users (
-#         id SERIAL NOT NULL,
-#         username VARCHAR(100) NOT NULL,
-#         age INTEGER,
-#         work VARCHAR,
-#         PRIMARY KEY (id)
-#     )
-#     """
-#
-#
-# class PyProduct(Base):
-#     __tablename__ = 'sqla_product'
-#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-#     name: Mapped[str] = mapped_column(types.String(100), nullable=False)
-#     price: Mapped[float] = mapped_column(nullable=False)
-#     available_quantity: Mapped[int] = mapped_column(nullable=False)
-#
-#     production_date: Mapped[date] = mapped_column(types.Date, nullable=True)
-#     expiry_date: Mapped[date] = mapped_column(types.Date, nullable=True)
-#     expiry_offset_months: Mapped[int] = mapped_column(nullable=True)  # Example: 12 for a year
-#
-#
-# class PyOrder(Base):
-#     __tablename__ = 'sqla_order'
-#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-#     quantity: Mapped[int] = mapped_column(nullable=False)
-#     order_date: Mapped[datetime] = mapped_column(default=datetime.utcnow)
-#     product_id: Mapped[int] = mapped_column(ForeignKey(PyProduct.id, ondelete="CASCADE"))
-#     """
-#     CREATE TABLE sqla_order (
-#         id SERIAL NOT NULL,
-#         quantity INTEGER NOT NULL,
-#         order_date TIMESTAMP WITH TIME ZONE NOT NULL,
-#         product_id INTEGER NOT NULL,
-#         PRIMARY KEY (id),
-#         FOREIGN KEY(product_id) REFERENCES sqla_product (id) ON DELETE CASCADE
-#     )
-#     """
+class PyUser(Base):
+    # this is how we create table for sqlalchemy version >= 2.0
+    __tablename__ = 'sqla_user'  # psql thinks table names are all lowercase so if you write uppercase letters it cant find them , so alway write tbale names in lower case
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)  # you should use python types
+    username: Mapped[str] = mapped_column(types.String(100))  # VARCHAR(100)
+    # 3 way to make a column nullable
+    # age: Mapped[Union[int | None]] = mapped_column()
+    age: Mapped[int | None] = mapped_column()
+    work: Mapped[str] = mapped_column(nullable=True)  # TEXT
 
-# class UserWithIndex(Base):
-#     __tablename__ = "users_with_index"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     email: Mapped[str] = mapped_column(types.String(255), nullable=False)
-#
-#     __table_args__ = (
-#         Index("idx_customers_email", "email"),
-#         # This creates an index on the email column , the index name is idx_customers_email
-#     )
-#     """
-#     two sql commands will run in one transaction:
-#
-#     CREATE TABLE users_with_index (
-#         id SERIAL NOT NULL,
-#         email VARCHAR(255) NOT NULL,
-#         PRIMARY KEY (id)
-#     )
-#
-#
-#     CREATE INDEX idx_customers_email ON users_with_index (email)
-#
-#     and this because i use the .begin() method and psql  . for example mysql will run them sequentially
-#     """
+    def to_dict(self):
+        # when i have obj which its type is obj of PyUser if we write obj.__dict__ we dont get a human-readable dict
+        # so we write this method and we will call it like this :obj.to_dict this is a python method inside a python
+        # class and do not relate to sqlalchemy and wont affect the table creation
+        return {
+            "id": self.id,
+            "username": self.username,
+            "age": self.age,
+            "work": self.work
+        }
+
+    """
+    CREATE TABLE users (
+        id SERIAL NOT NULL,
+        username VARCHAR(100) NOT NULL,
+        age INTEGER,
+        work VARCHAR,
+        PRIMARY KEY (id)
+    )
+    """
+
+
+class PyProduct(Base):
+    __tablename__ = 'sqla_product'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(types.String(100), nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    available_quantity: Mapped[int] = mapped_column(nullable=False)
+
+    production_date: Mapped[date] = mapped_column(types.Date, nullable=True)
+    expiry_date: Mapped[date] = mapped_column(types.Date, nullable=True)
+    expiry_offset_months: Mapped[int] = mapped_column(nullable=True)  # Example: 12 for a year
+
+
+class PyOrder(Base):
+    __tablename__ = 'sqla_order'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    order_date: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    product_id: Mapped[int] = mapped_column(ForeignKey(PyProduct.id, ondelete="CASCADE"))
+    """
+    CREATE TABLE sqla_order (
+        id SERIAL NOT NULL,
+        quantity INTEGER NOT NULL,
+        order_date TIMESTAMP WITH TIME ZONE NOT NULL,
+        product_id INTEGER NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY(product_id) REFERENCES sqla_product (id) ON DELETE CASCADE
+    )
+    """
+
+
+class UserWithIndex(Base):
+    __tablename__ = "users_with_index"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(types.String(255), nullable=False)
+
+    __table_args__ = (
+        Index("idx_customers_email", "email"),
+        # This creates an index on the email column , the index name is idx_customers_email
+    )
+    """
+    two sql commands will run in one transaction:
+
+    CREATE TABLE users_with_index (
+        id SERIAL NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    )
+
+
+    CREATE INDEX idx_customers_email ON users_with_index (email)
+
+    and this because i use the .begin() method and psql  . for example mysql will run them sequentially
+    """
 
 
 class ProductWithCompositeUniqueness(Base):
@@ -99,5 +110,3 @@ class ProductWithCompositeUniqueness(Base):
         CONSTRAINT uq_product_supplier UNIQUE (name, supplier_id)
     )
     """
-
-
