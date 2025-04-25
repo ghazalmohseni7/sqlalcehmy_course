eeba0c85ec51618cdc17403d4b22636951cdc8f4
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2025-04-25 16:53:53 +0330

    fix : solve the problem of class test

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -7,18 +7,18 @@ from models import *
 fake = Faker()
 
 
-# class TestInsert: # when it was inside the class type(async_session1) was  instance of TestInsert, i donw know why
-@pytest.mark.asyncio
-async def test_insert_category(async_session1):
-    print(1111111111111111111111111111, type(async_session1))
-    import asyncio
-    loop = asyncio.get_event_loop()
-    print(
-        f"***************************************************Event Loop ID (Fixture test_insert_category): {id(loop)}")
-  
-    new_cat = Category(name=fake.word())
-    async_session1.add(new_cat)
-    await async_session1.commit()
-    await async_session1.refresh(new_cat)
+class TestInsert:
+    @pytest.mark.asyncio
+    async def test_insert_category(self, async_session1):
+        print(1111111111111111111111111111, type(async_session1))
+        import asyncio
+        loop = asyncio.get_event_loop()
+        print(
+            f"***************************************************Event Loop ID (Fixture test_insert_category): {id(loop)}")
 
-    assert new_cat.id is not None
+        new_cat = Category(name=fake.word())
+        async_session1.add(new_cat)
+        await async_session1.commit()
+        await async_session1.refresh(new_cat)
+
+        assert new_cat.id is not None
